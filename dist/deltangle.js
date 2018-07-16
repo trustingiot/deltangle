06db2a1b72b129dffa2db27e98a3f86ae02e2056
William Wyatt <tsangares@gmail.com>
2018-07-16 00:39:27 -0700

    Update deltangle.js

--- dist/deltangle.js
+++ dist/deltangle.js
@@ -19,7 +19,7 @@ class Deltangle {
         async post(content, previous = null) {
 	        this.iota.api.getNodeInfo((err,sucess) => {
 			if(err) {
-				console.log("\nApi not working; Connect to a diffierent host.")
+				console.log("\nApi not working; Connect to a diffierent host using the flag --provider.")
 				process.exit()
 			}
 		})
